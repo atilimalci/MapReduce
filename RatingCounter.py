a6b8a04e26405a02cba821c7ee3c4795145e652f
ATILIM ALCI <alciatilim@gmail.com>
2021-03-27 22:50:35 +0300

    First commit

---
+++ RatingCounter.py
@@ -0,0 +1,13 @@
+#Works with Mrjob 0.6.12
+from mrjob.job import MRJob
+
+class MRRatingCounter(MRJob):
+    def mapper(self, key, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield rating, 1
+
+    def reducer(self, rating, occurences):
+        yield rating, sum(occurences)
+
+if __name__ == '__main__':
+    MRRatingCounter.run()
